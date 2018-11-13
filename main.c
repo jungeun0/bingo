a096cd86366189cf643e6f0996956ffebeeaa1b1
jungeun0 <sky_fall7@naver.com>
2018-11-13 15:28:38 +0900

    .

--- main.c
+++ main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 3
+#define N 3 //1-1
+#define M 3	//1-2
 
-/* 
-
-	빙고판 만들기
-		1.  N값을 입력받음 --> 기호 상수로 입력 받아서 빙고판 만들 때 활용
-		2.  M값을 입력받음 --> 기호 상수로 입력 받아서 줄 수로 활용
-		3.  N^N 배열의 이차원 배열 생성, 랜덤으로 숫자 1~N^2까지 채워넣기
-			--> 랜덤으로 수를 채워넣기
-		4.  같은 작업을 한번 더 반복 --> 컴퓨터의 빙고판 생성
-		
+	/* 
+	
 	빙고 게임 시작
 		1. 빙고판  상태를 화면에 표시
 		2. 숫자를 선택하라고 출력하고, 나중에 숫자를 입력
@@ -19,14 +13,11 @@
 		5. 3의 동작 반복 			*/
 		
 void initate_bingo(int MyBingo[N][N],int NotMyBingo[N][N]){
-
+	int a,b;
+	
 	make_bingo(MyBingo[N][N]);
 	
 	make_bingo(NotMyBingo[N][N]);
-	
-	//랜덤으로 발생한 빙고판을 프린트함 -->만들면서 바로 프린트? 
-	
-	return;
 	
 	}
 
@@ -36,13 +27,13 @@ int make_bingo(int MyBingo[N][N],int NotMyBingo[N][N]){
 		
 		srand((unsigned int)time(NULL));
 		
-		//
+		//중복되지 않는 난수 생성..........! 
 		for (i=0;i<N;i++) {
 			for(j=0;j<N;j++){
 				MyBingo[i][j] = rand() % (N^2);
 				}	
 		}
-
+		//1-3
 		for(i=0;i<N;i++) {
 			for(j=0;j<N;j++){
 			NotMyBingo[i][j] = rand() % (N^2);
@@ -50,7 +41,12 @@ int make_bingo(int MyBingo[N][N],int NotMyBingo[N][N]){
           }
 
 
-}	
+}	/*1. 빙고판 만들기
+		1.  N값을 입력받음 --> 기호 상수로 입력 받아서 빙고판 만들 때 활용
+		2.  M값을 입력받음 --> 기호 상수로 입력 받아서 줄 수로 활용
+		3.  N^N 배열의 이차원 배열 생성, 랜덤으로 숫자 1~N^2까지 채워넣기
+			--> 랜덤으로 수를 채워넣기
+		4.  같은 작업을 한번 더 반복 --> 컴퓨터의 빙고판 생성*/
 
 int main(int argc, char *argv[]) {
 
