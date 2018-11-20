fdbb36126eafa6f958c3f9d52a6f723465b34aa5
jungeun0 <sky_fall7@naver.com>
2018-11-20 16:20:36 +0900

    .

--- count_bingo.c
+++ count_bingo.c
@@ -47,7 +47,7 @@ int count_bingo(int Z[N][N]){
 	}//j가 일정할 때,0의 갯수를 세서 0이 N개일 경우, 가로줄 1줄추가
 	
 	
-	for(i=0,j=0;i<N,j<N;i++,j++){
+	for(i=0,j=0;i<N&&j<N;i++,j++){
 			if(Z[i][j]==0){
 				score++;
 			}
@@ -60,13 +60,13 @@ int count_bingo(int Z[N][N]){
 	else{
 		score = 0;
 	}
-	
-	for(i=N,j=N;i>0,j>0;i--,j--){
-		if(Z[i][j]==0){
-			score;
+	//for문을 이용해 왼쪽에서 오른쪽으로 내려오는 대각선 줄이 생성되었는지 확인
+	for(i=0,j=N-1;i<N&&j>=0;i++,j--){
+			if(Z[i][j]==0){
+				score++;
+			}
 		}
-	}
-	
+	//i는 0의 인덱스부터, j는 N-1의 인덱스부터 for문을 돌려 오른쪽에서 왼쪽으로 내려오는 대각선 줄이 생성되었는지 확인 
 	if(score == N){
 		res++;
 		score = 0;
--- main.c
+++ main.c
@@ -9,15 +9,14 @@
 #include "get_number_byCom.h"
  
 int main(int argc, char *argv[]){
-	
-	int k=0;
+    int k=0;
 	int j=0;
 	int n = 1;
 	int MyBingoNum = 0;
 	int ComBingoNum = 0;
 	int MyBingo[N][N];
 	int ComBingo[N][N];
-	int MIAOW[N*N] = {0};    //빙고게임을 하며 숫자를 입력받을 때 중복된 수인지 아닌지 확인하는 배열 
+	int MIAOW[N*N] = {0};  //빙고게임을 하며 숫자를 입력받을 때 중복된 수인지 아닌지 확인하는 배열 
 
 	srand((unsigned int)time(NULL));
 	
@@ -44,81 +43,87 @@ int main(int argc, char *argv[]){
 		printf("%d 번째 판입니다. \n",n); 
 		while(1){
 			k = get_number_byMe(); //사용자에게 받은 값을 변수에 저장 
-			if(MIAOW[k]==0){ //처음 받는 변수인 경우 
-				MIAOW[k] = 1;  //변수를 받았다고 표시 
+			if(MIAOW[k-1]==0){ //처음 받는 변수인 경우 
+				MIAOW[k-1] = 1;  //변수를 받았다고 표시 
 				break;
 			}
-			else if (MIAOW[k]== 1){
-				printf("이미 선택된 수입니다. 다른 수를 선택해주세요.\n");
-			}
+			else if (MIAOW[k-1] == 1){
+				printf("\n이미 선택된 수입니다. 다른 수를 선택해주세요.\n");
+			} // k-1번째 배열이 이미 1이라면, k는 이미 선택된 숫자 
 			
 		}
 		
 		process_bingo(MyBingo,ComBingo,k);
 		
-		printf("나의 빙고판 \n");
+		printf("\n나의 빙고판 \n");
 		
 		print_bingo(MyBingo);
 		
-		printf("상대방의 빙고판 \n");
+		printf("\n상대방의 빙고판 \n");
 		
 		print_bingo(ComBingo); 
 		
-		
 		MyBingoNum = count_bingo(MyBingo);
 		
-		printf("내 빙고의 줄 수는 %d\n",MyBingoNum);
+		printf("\n내 빙고의 줄 수는 %d\n",MyBingoNum);
 		
 		ComBingoNum = count_bingo(ComBingo);
 		
-		printf("상대방 빙고의 줄 수는 %d\n",ComBingoNum); 
+		printf("\n상대방 빙고의 줄 수는 %d\n",ComBingoNum); 
+	
 		
 		if (MyBingoNum == M){
-			printf("승리하셨습니다 (시도횟수 : %d ) !\n",n);
+			printf("\n승리하셨습니다 (시도횟수 : %d ) !\n",n);
 			break;
 		}
-		else if (ComBingo == M){
-			printf("패배하셨습니다(시도횟수 : %d ) !\n", n );
+		else if (ComBingoNum == M){
+			printf("\n패배하셨습니다(시도횟수 : %d ) !\n", n );
 			break; 
 		}
+		else if (ComBingoNum == M && MyBingoNum == M){
+			printf("\n 비겼습니다(시도횟수 : %d ) !\n",n);
+			break;
+		} 
 		 
 		while(1){
 			k = get_number_byCom();
-			if(MIAOW[k]==0){
-				MIAOW[k] = 1;
+			if(MIAOW[k-1]==0){
+				MIAOW[k-1] = 1;
 				break;
 			}
 		}
 		
-		void turn_function();
+		
+		printf("컴퓨터가 선택한 수는 %d \n",k);
 		
 		process_bingo(MyBingo,ComBingo,k);
 		
-		printf("   나의 빙고판 \n");
+		printf("\n나의 빙고판 \n");
 		
 		print_bingo(MyBingo);
 		
-		printf("   상대방의 빙고판 \n");
+		printf("\n상대방의 빙고판 \n");
 		
 		print_bingo(ComBingo); 
 		
+		
 		MyBingoNum = count_bingo(MyBingo);
 		
-		printf("내 빙고의 줄 수는 %d\n",MyBingoNum);
+		printf("\n내 빙고의 줄 수는 %d\n",MyBingoNum);
 		
 		ComBingoNum = count_bingo(ComBingo);
 		
-		printf("상대방 빙고의 줄 수는 %d\n",ComBingoNum);
+		printf("\n상대방 빙고의 줄 수는 %d\n",ComBingoNum); 
+	
 		
 		if (MyBingoNum == M){
-			printf("승리하셨습니다 (시도횟수 : %d ) !\n",n);
+			printf("\n승리하셨습니다 (시도횟수 : %d ) !\n",n);
 			break;
 		}
-		else if (ComBingo == M){
-			printf("패배하셨습니다(시도횟수 : %d ) !\n",n );
+		else if (ComBingoNum == M){
+			printf("\n패배하셨습니다(시도횟수 : %d ) !\n", n );
 			break; 
-		} 
-		
+		}
 		 
 		n++;  //빙고가 몇 번째 판인지 세는 변수  
 		
